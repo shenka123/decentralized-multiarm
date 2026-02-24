28d5b00ee7990e1b8a8581646155763b9dff2222
shenka123 <b.sashenka99@gmail.com>
2026-02-24 19:17:27 +0800

    More frequent checkpoint saves

---
+++ scripts/job_birrt_10.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=obstacle_gen
+#SBATCH --output=logs/obstacle_%j.out
+#SBATCH --error=logs/obstacle_%j.err
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=06:00:00
+
+# ===== Load Conda =====
+source ~/.bashrc
+
+# Activate conda environment
+conda activate multiarm
+
+# Move to project directory
+cd ~/decentralized-multiarm
+
+# Run your script
+python obstacleGenerator.py base obstacle_v1 2
---
+++ scripts/job_train_v1 copy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=decentralized_multiarm_train
+#SBATCH --output=logs/train_%j.out
+#SBATCH --error=logs/train_%j.err
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=06:00:00
+
+source ~/.bashrc
+
+conda activate multiarm
+cd ~/decentralized-multiarm
+
+python main.py \
+  --mode train \
+  --name obstacle_v1 \
+  --config configs/obstacle_v1.json \
+  --tasks_path tasks/obstacle_v1 \
+  --expert_waypoints experts/obstacle_v1 \
+  --num_processes 8 \
+  --load runs/obstacle_v1/sckpt_multiarm_motion_planner_00002
