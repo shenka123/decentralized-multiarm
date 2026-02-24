12a54b3a795463d09cf1f5ed9f73e424394eb180
shenka123 <b.sashenka99@gmail.com>
2026-02-24 19:18:58 +0800

    fix script

--- scripts/job_train_v1.sh
+++ scripts/job_train_v1.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=logs/train_%j.out
 #SBATCH --error=logs/train_%j.err
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=10
 #SBATCH --time=06:00:00
 
 source ~/.bashrc
@@ -17,4 +17,5 @@ python main.py \
   --config configs/obstacle_v1.json \
   --tasks_path tasks/obstacle_v1 \
   --expert_waypoints experts/obstacle_v1 \
-  --num_processes 8
+  --num_processes 8 \
+  --load runs/obstacle_v1/sckpt_multiarm_motion_planner_00002
