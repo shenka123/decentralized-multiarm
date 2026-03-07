b858120222b1096ffad06183837482b91d575beb
shenka123 <b.sashenka99@gmail.com>
2026-03-08 01:13:29 +0800

    debugging

--- obstacleGenerator.py
+++ obstacleGenerator.py
@@ -25,18 +25,16 @@ def iter_tasks(tasks_dir, logs):
     for task_file in iter_json_files(tasks_dir):
         
         filename = os.path.basename(task_file)
-        if filename in logs["runs"]:
-            continue
-        try:
-            print(filename)
-            with open(task_file) as f:
-                task_data = json.load(f)
-            yield filename, task_data, task_file
-        except Exception as e:
-            print(f"[ERROR] Could not load {filename}: {e}")
-            logs["runs"][filename] = "error:0"
-            logs["stats"]["processed"] += 1
-            logs["stats"]["errors"] += 1
+        if filename not in logs["runs"]:
+            try:
+                with open(task_file) as f:
+                    task_data = json.load(f)
+                yield filename, task_data, task_file
+            except Exception as e:
+                print(f"[ERROR] Could not load {filename}: {e}")
+                logs["runs"][filename] = "error:0"
+                logs["stats"]["processed"] += 1
+                logs["stats"]["errors"] += 1
 
 
 def dump_json(dics, filename, save_write=False):
