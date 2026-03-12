148d0fec2f368f0e47e82e3022898af215176f88
shenka123 <b.sashenka99@gmail.com>
2026-03-12 11:03:19 +0800

    test

--- test.py
+++ test.py
@@ -5,15 +5,6 @@ import torch
 
 if __name__ == "__main__":
     
-    d = 'runs/obstacle_v1/ckpt_multiarm_motion_planner_'
-    a = torch.load(d + "00001")
-    b = torch.load(d + "00013")
-
-    for k in a['networks'].keys():
-        for k2 in a['networks'][k]:
-            if not torch.equal(a['networks'][k][k2], b['networks'][k][k2]):
-                print("Different at:", k + '.' + k2)
-            else:
-                print(k + '.' + k2, "is equal.")
-    print(a['networks'].keys())
-    print(a['stats'], b['stats'])
+    if not torch.cuda.is_available():
+        print("Using CPU")
+    print("Using GPU")
