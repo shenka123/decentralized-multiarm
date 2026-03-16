53e3fce2ef0634626162ba47eae84c73091cf344
shenka123 <b.sashenka99@gmail.com>
2026-03-16 15:47:19 +0800

    add cuda config

--- scripts/job_train_v2_1.sh
+++ scripts/job_train_v2_1.sh
@@ -3,12 +3,14 @@
 #SBATCH --output=logs/train_v2_%j.out
 #SBATCH --error=logs/train_v2_%j.err
 #SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=10
 #SBATCH --time=06:00:00
 
 source ~/.bashrc
 
 conda activate multiarm
+module load cuda/12.8.0 
 cd ~/decentralized-multiarm
 
 python main.py \
--- scripts/job_train_v2_3.sh
+++ scripts/job_train_v2_3.sh
@@ -3,6 +3,7 @@
 #SBATCH --output=logs/train_v2_%j.out
 #SBATCH --error=logs/train_v2_%j.err
 #SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=10
 #SBATCH --time=06:00:00
 
