2a7717e34f835d252ac881adbc1e08a99d485ee1
shenka123 <b.sashenka99@gmail.com>
2026-05-22 17:04:36 +0800

    Dense environment

---
+++ scripts/job_train_v4_ext.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=dma-train
+#SBATCH --partition=MGPU-TC2
+#SBATCH --qos=q_m1x24
+#SBATCH --output=logs/train_v4_%j.out
+#SBATCH --error=logs/train_v4_%j.err
+#SBATCH --ntasks=1
+#SBATCH --nodelist=TC2N08
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=24:00:00
+
+source ~/.bashrc
+
+conda activate multiarm
+cd ~/decentralized-multiarm
+
+
+python main.py \
+  --mode train \
+  --name obstacle_v4 \
+  --config configs/obstacle_v1.json \
+  --tasks_path tasks/obstacle_v1 \
+  --expert_waypoints experts/obstacle_v1/ \
+  --max_time 24 \
+  --num_processes 10 
